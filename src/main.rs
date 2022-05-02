df9e65e30acf50bbc2749dc45e18253e30f3be80
nulvox <38874956+nulvox@users.noreply.github.com>
2022-05-02 06:05:03 -0400

    Initial pin definitions
     * define pins names
     * set I/O modes
     * build function headers for processing
     * establish input modes
     * debounce inputs
     * define button masks
     * roughly define loop logic

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,6 @@
 
 use panic_halt as _;
 
-// Button and light pinout
-static REDLIGHT: u16 13
-static BLUELIGHT: u16 4 
-static BUTTONA: u16 3
-static BUTTONB: u16 19
-static BUTTONX: u16 18
-static BUTTONY: u16 15
-static BUTTONL1: u16 19
-static BUTTONR1: u16 5
-static BUTTONL2: u16 20
-static BUTTONR2: u16 0
-static BUTTONSELECT: u16 14
-static BUTTONSTART: u16 10
-static BUTTONUP: u16 7
-static BUTTONDOWN: u16 8
-static BUTTONLEFT: u16 6
-static BUTTONRIGHT: u16 9
-static BUTTONSHIFT: u16 2
-static BUTTONHOME: u16 16
-
 // Button state masks
 static MASK_A: u16 0x0004
 static MASK_B: u16 0x0002
@@ -37,36 +17,158 @@ static MASK_START: u16 0x0200
 static MASK_HOME: u16 0x1000
 
 // Dpad Hat switch state masks
-static MASK_UP: u16 0x0000
-static MASK_UPRIGHT: u16 0x0001
-static MASK_RIGHT: u16 0x0002
-static MASK_DOWNRIGHT: u16 0x0003
-static MASK_DOWN: u16 0x0004
-static MASK_DOWNLEFT: u16 0x0005
-static MASK_LEFT: u16 0x0006
-static MASK_UPLEFT: u16 0x0007
-static MASK_NONE: u16 0x0008
+static PAD_MASK_UP: u16 0x0000
+static PAD_MASK_UPRIGHT: u16 0x0001
+static PAD_MASK_RIGHT: u16 0x0002
+static PAD_MASK_DOWNRIGHT: u16 0x0003
+static PAD_MASK_DOWN: u16 0x0004
+static PAD_MASK_DOWNLEFT: u16 0x0005
+static PAD_MASK_LEFT: u16 0x0006
+static PAD_MASK_UPLEFT: u16 0x0007
+static PAD_MASK_NONE: u16 0x0008
 
+// Mode Selection
+enum InputMode {
+    Dpad,
+    Analog,
+    Smash,
+}
+
+// Swap Input mode by pressing HOME and SHIFT
+fn checkModeChange (&mut mode: InputMode, &mut changed: bool) {
+    if ( !changed && BUTTONSHIFT.pressed() && BUTTONHOME.pressed() ){
+        match mode {
+            Dpad => {
+                mode = Analog;
+                redlight.set_high();
+                bluelight.set_high();
+            },
+            Analog => {
+                mode = Smash;
+                redlight.set_high();
+                bluelight.set_low();
+            },
+            Smash => {
+                mode = Dpad;
+                redlight.set_low();
+                bluelight.set_high();
+            },
+        }
+        let changed = true;
+    } else {
+        changed = false;
+    }
+}
+
+fn definePins(&mut pins: arduino_hal::gpio::Pins) {
+    // Return a struct with named objects
+    return enum {
+        REDLIGHT: pins.a3,
+        BLUELIGHT: pins.d4,
+        BUTTONA: pins.d3,
+        BUTTONB: pins.a1,
+        BUTTONX: pins.a0,
+        BUTTONY: pins.d15,
+        BUTTONL1: pins.d19,
+        BUTTONR1: pins.d5,
+        BUTTONL2: pins.a2,
+        BUTTONR2: pins.d0,
+        BUTTONSELECT: pins.d14,
+        BUTTONSTART: pins.d10,
+        BUTTONUP: pins.d7,
+        BUTTONDOWN: pins.d8,
+        BUTTONLEFT: pins.d6,
+        BUTTONRIGHT: pins.d9,
+        BUTTONSHIFT: pins.d2,
+        BUTTONHOME: pins.d16,
+    }
+}
+
+fn processSmash(& pins: arduino_hal::gpio::Pins) {
+    let mut report = PAD_MASK_NONE;
+    // Reat the directions as analog input
+    // shift makes half values
+}
+
+fn processAnalog(& pins: arduino_hal::gpio::Pins) {
+    let mut report = PAD_MASK_NONE;
+    // Reat the directions as analog input
+    // shift makes the input register right stick
+}
+
+fn processDpad(& pins: arduino_hal::gpio::Pins) {
+    let mut report = PAD_MASK_NONE;
+    // Reat the directions as digital input
+    // shift makes the input register SOCD... ish
+}
+
+fn processButtons(& pins: arduino_hal::gpio::Pins) {
+    // learn to read buttons... for all of this
+
+}
+
+fn buttonRead(& pins: arduino_hal::gpio::Pins) {
+    // read buttons
+    // if button is pressed, set the bit
+    // if button is released, clear the bit
+}
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    
+    let enum pinnames = definePins(mut &pins).unwrap();
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    // Setup pin modes
+    let mut redlight = pinnames.REDLIGHT.into_output();
+    let mut bluelight = pinnames.BLUELIGHT.into_output();
+    let mut buttona = pinnames.BUTTONA.into_float();
+    let mut buttonb = pinnames.BUTTONB.into_float();
+    let mut buttonx = pinnames.BUTTONX.into_float();
+    let mut buttony = pinnames.BUTTONY.into_float();
+    let mut buttonl1 = pinnames.BUTTONL1.into_float();
+    let mut buttonr1 = pinnames.BUTTONR1.into_float();
+    let mut buttonl2 = pinnames.BUTTONL2.into_float();
+    let mut buttonr2 = pinnames.BUTTONR2.into_float();
+    let mut buttonselect = pinnames.BUTTONSELECT.into_float();
+    let mut buttonstart = pinnames.BUTTONSTART.into_float();
+    let mut buttonup = pinnames.BUTTONUP.into_float();
+    let mut buttondown = pinnames.BUTTONDOWN.into_float();
+    let mut buttonleft = pinnames.BUTTONLEFT.into_float();
+    let mut buttonright = pinnames.BUTTONRIGHT.into_float();
+    let mut buttonshift = pinnames.BUTTONSHIFT.into_float();
+    let mut buttonhome = pinnames.BUTTONHOME.into_float();
 
-    let mut led = pins.d13.into_output();
+    // Setup debounce
+    let mut debouncea = Debounce::new(buttona, 1);
+    let mut debounceb = Debounce::new(buttonb, 1);
+    let mut debouncex = Debounce::new(buttonx, 1);
+    let mut debouncey = Debounce::new(buttony, 1);
+    let mut debouncel1 = Debounce::new(buttonl1, 1);
+    let mut debouncer1 = Debounce::new(buttonr1, 1);
+    let mut debouncel2 = Debounce::new(buttonl2, 1);
+    let mut debouncer2 = Debounce::new(buttonr2, 1);
+    let mut debounceselect = Debounce::new(buttonselect, 1);
+    let mut debouncestart = Debounce::new(buttonstart, 1);
+    let mut debounceup = Debounce::new(buttonup, 1);
+    let mut debouncedown = Debounce::new(buttondown, 1);
+    let mut debounceleft = Debounce::new(buttonleft, 1);
+    let mut debounceright = Debounce::new(buttonright, 1);
+    let mut debouncehome = Debounce::new(buttonhome, 1);
+    let mut debounceshift = Debounce::new(buttonshift, 1);
+
+    // Set the initial state of the LEDs and input mode
+    redlight.set_high();
+    bluelight.set_high();
+    let mut mode = Dpad;
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
+        buttonRead();
+        checkModeChange();
+        processButtons();
+        HID_Task();
+        // Update the USB HID report
+        USB_USBTask();
     }
 }
