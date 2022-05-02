468bc103f2c40f8969566aefe1391624d026f169
nulvox <38874956+nulvox@users.noreply.github.com>
2022-05-02 06:53:13 -0400

    Update debouncing code to use flapper

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![no_std]
 #![no_main]
 
+use unflappable::{debouncer_uninit, Debouncer, default::ActiveLow};
 use panic_halt as _;
+
+// Initialize the debouncer
+static DEBOUNCER: Debouncer<PinType, ActiveLow> = debouncer_uninit!();
 
 // Button state masks
 static MASK_A: u16 0x0004
@@ -26,6 +30,9 @@ static PAD_MASK_DOWNLEFT: u16 0x0005
 static PAD_MASK_LEFT: u16 0x0006
 static PAD_MASK_UPLEFT: u16 0x0007
 static PAD_MASK_NONE: u16 0x0008
+
+// Set bounce time in ms
+static BOUNCE_TIME: u16 1
 
 // Mode Selection
 enum InputMode {
@@ -55,50 +62,52 @@ fn checkModeChange (&mut mode: InputMode, &mut changed: bool) {
             },
         }
         let changed = true;
+        return mode
     } else {
         changed = false;
+        return mode
     }
 }
 
 fn definePins(&mut pins: arduino_hal::gpio::Pins) {
     // Return a struct with named objects
     return enum {
-        REDLIGHT: pins.a3,
-        BLUELIGHT: pins.d4,
-        BUTTONA: pins.d3,
-        BUTTONB: pins.a1,
-        BUTTONX: pins.a0,
-        BUTTONY: pins.d15,
-        BUTTONL1: pins.d19,
-        BUTTONR1: pins.d5,
-        BUTTONL2: pins.a2,
-        BUTTONR2: pins.d0,
-        BUTTONSELECT: pins.d14,
-        BUTTONSTART: pins.d10,
-        BUTTONUP: pins.d7,
-        BUTTONDOWN: pins.d8,
-        BUTTONLEFT: pins.d6,
-        BUTTONRIGHT: pins.d9,
-        BUTTONSHIFT: pins.d2,
-        BUTTONHOME: pins.d16,
+        RedLight: pins.a3,
+        BlueLight: pins.d4,
+        ButtonA: pins.d3,
+        ButtonB: pins.a1,
+        ButtonX: pins.a0,
+        ButtonY: pins.d15,
+        ButtonL1: pins.d19,
+        ButtonR1: pins.d5,
+        ButtonL2: pins.a2,
+        ButtonR2: pins.d0,
+        ButtonSELECT: pins.d14,
+        ButtonSTART: pins.d10,
+        ButtonUP: pins.d7,
+        ButtonDOWN: pins.d8,
+        ButtonLEFT: pins.d6,
+        ButtonRIGHT: pins.d9,
+        ButtonSHIFT: pins.d2,
+        ButtonHOME: pins.d16,
     }
 }
 
 fn processSmash(& pins: arduino_hal::gpio::Pins) {
     let mut report = PAD_MASK_NONE;
-    // Reat the directions as analog input
+    // Treat the directions as analog input
     // shift makes half values
 }
 
 fn processAnalog(& pins: arduino_hal::gpio::Pins) {
     let mut report = PAD_MASK_NONE;
-    // Reat the directions as analog input
+    // Treat the directions as analog input
     // shift makes the input register right stick
 }
 
 fn processDpad(& pins: arduino_hal::gpio::Pins) {
     let mut report = PAD_MASK_NONE;
-    // Reat the directions as digital input
+    // Treat the directions as digital input
     // shift makes the input register SOCD... ish
 }
 
@@ -118,45 +127,47 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
     
-    let enum pinnames = definePins(mut &pins).unwrap();
+    let enum pinnames = definePins(mut &pins);
 
     // Setup pin modes
-    let mut redlight = pinnames.REDLIGHT.into_output();
-    let mut bluelight = pinnames.BLUELIGHT.into_output();
-    let mut buttona = pinnames.BUTTONA.into_float();
-    let mut buttonb = pinnames.BUTTONB.into_float();
-    let mut buttonx = pinnames.BUTTONX.into_float();
-    let mut buttony = pinnames.BUTTONY.into_float();
-    let mut buttonl1 = pinnames.BUTTONL1.into_float();
-    let mut buttonr1 = pinnames.BUTTONR1.into_float();
-    let mut buttonl2 = pinnames.BUTTONL2.into_float();
-    let mut buttonr2 = pinnames.BUTTONR2.into_float();
-    let mut buttonselect = pinnames.BUTTONSELECT.into_float();
-    let mut buttonstart = pinnames.BUTTONSTART.into_float();
-    let mut buttonup = pinnames.BUTTONUP.into_float();
-    let mut buttondown = pinnames.BUTTONDOWN.into_float();
-    let mut buttonleft = pinnames.BUTTONLEFT.into_float();
-    let mut buttonright = pinnames.BUTTONRIGHT.into_float();
-    let mut buttonshift = pinnames.BUTTONSHIFT.into_float();
-    let mut buttonhome = pinnames.BUTTONHOME.into_float();
+    let mut redlight = pinnames.RedLight.into_output();
+    let mut bluelight = pinnames.BlueLight.into_output();
+    let mut buttona = pinnames.ButtonA.into_float();
+    let mut buttonb = pinnames.ButtonB.into_float();
+    let mut buttonx = pinnames.ButtonX.into_float();
+    let mut buttony = pinnames.ButtonY.into_float();
+    let mut buttonl1 = pinnames.ButtonL1.into_float();
+    let mut buttonr1 = pinnames.ButtonR1.into_float();
+    let mut buttonl2 = pinnames.ButtonL2.into_float();
+    let mut buttonr2 = pinnames.ButtonR2.into_float();
+    let mut buttonselect = pinnames.ButtonSELECT.into_float();
+    let mut buttonstart = pinnames.ButtonSTART.into_float();
+    let mut buttonup = pinnames.ButtonUP.into_float();
+    let mut buttondown = pinnames.ButtonDOWN.into_float();
+    let mut buttonleft = pinnames.ButtonLEFT.into_float();
+    let mut buttonright = pinnames.ButtonRIGHT.into_float();
+    let mut buttonshift = pinnames.ButtonSHIFT.into_float();
+    let mut buttonhome = pinnames.ButtonHOME.into_float();
 
-    // Setup debounce
-    let mut debouncea = Debounce::new(buttona, 1);
-    let mut debounceb = Debounce::new(buttonb, 1);
-    let mut debouncex = Debounce::new(buttonx, 1);
-    let mut debouncey = Debounce::new(buttony, 1);
-    let mut debouncel1 = Debounce::new(buttonl1, 1);
-    let mut debouncer1 = Debounce::new(buttonr1, 1);
-    let mut debouncel2 = Debounce::new(buttonl2, 1);
-    let mut debouncer2 = Debounce::new(buttonr2, 1);
-    let mut debounceselect = Debounce::new(buttonselect, 1);
-    let mut debouncestart = Debounce::new(buttonstart, 1);
-    let mut debounceup = Debounce::new(buttonup, 1);
-    let mut debouncedown = Debounce::new(buttondown, 1);
-    let mut debounceleft = Debounce::new(buttonleft, 1);
-    let mut debounceright = Debounce::new(buttonright, 1);
-    let mut debouncehome = Debounce::new(buttonhome, 1);
-    let mut debounceshift = Debounce::new(buttonshift, 1);
+    // Setup debounce enum
+    let mut debouncebuttons = enum {
+        debouncea = unsafe { DEBOUNCER.init(buttona) }?;
+        debounceb = unsafe { DEBOUNCER.init(buttonb) }?;
+        debouncex = unsafe { DEBOUNCER.init(buttonx) }?;
+        debouncey = unsafe { DEBOUNCER.init(buttony) }?;
+        debouncel1 = unsafe { DEBOUNCER.init(buttonl1) }?;
+        debouncer1 = unsafe { DEBOUNCER.init(buttonr1) }?;
+        debouncel2 = unsafe { DEBOUNCER.init(buttonl2) }?;
+        debouncer2 = unsafe { DEBOUNCER.init(buttonr2) }?;
+        debounceselect = unsafe { DEBOUNCER.init(buttonselect) }?;
+        debouncestart = unsafe { DEBOUNCER.init(buttonstart) }?;
+        debounceup = unsafe { DEBOUNCER.init(buttonup) }?;
+        debouncedown = unsafe { DEBOUNCER.init(buttondown) }?;
+        debounceleft = unsafe { DEBOUNCER.init(buttonleft) }?;
+        debounceright = unsafe { DEBOUNCER.init(buttonright) }?;
+        debounceshift = unsafe { DEBOUNCER.init(buttonshift) }?;
+        debouncehome = unsafe { DEBOUNCER.init(buttonhome) }?;
+    }
 
     // Set the initial state of the LEDs and input mode
     redlight.set_high();
@@ -164,11 +175,17 @@ fn main() -> ! {
     let mut mode = Dpad;
 
     loop {
-        buttonRead();
-        checkModeChange();
-        processButtons();
-        HID_Task();
+        // Read what is pressed
+        // we might need to read the debounce objects instead
+        // those should be in an enum.
+        let mut buttonstate = buttonRead(debouncebuttons);
+        // Update input mode if requested
+        let mode = checkModeChange(&buttonstate);
+        // Process the input
+        let reportdata = processButtons(&buttonstate, mode);
         // Update the USB HID report
-        USB_USBTask();
+        let HIDreport = HID_Task(&reportdata);
+        // Ship the USB HID report
+        USB_USBTask(&HIDReport);
     }
 }
