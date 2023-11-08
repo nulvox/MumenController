80cdb4371f48796979651a569f00cf2783d67306
nulvox <38874956+nulvox@users.noreply.github.com>
2023-11-07 23:03:57 -0500

    Define first 3 keyspec files

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,42 @@
 #![no_main]
 
 use panic_halt as _;
+use arduino_hal;
+
+// Button state masks
+static MASK_A: u16 = 0x0004;
+static MASK_B: u16 = 0x0002;
+static MASK_X: u16 = 0x0008;
+static MASK_Y: u16 = 0x0001;
+static MASK_L1: u16 = 0x0010;
+static MASK_R1: u16 = 0x0020;
+static MASK_L2: u16 = 0x0040;
+static MASK_R2: u16 = 0x0080;
+static MASK_SELECT: u16 = 0x0100;
+static MASK_START: u16 = 0x0200;
+static MASK_HOME: u16 = 0x1000;
+static MASK_NONE: u16 = 0x0000;
+
+// Dpad Hat switch state masks
+static PAD_MASK_UP: u8 = 0x00;
+static PAD_MASK_UPRIGHT: u8 = 0x01;
+static PAD_MASK_RIGHT: u8 = 0x02;
+static PAD_MASK_DOWNRIGHT: u8 = 0x03;
+static PAD_MASK_DOWN: u8 = 0x04;
+static PAD_MASK_DOWNLEFT: u8 = 0x05;
+static PAD_MASK_LEFT: u8 = 0x06;
+static PAD_MASK_UPLEFT: u8 = 0x07;
+static PAD_MASK_NONE: u8 = 0x08;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+//     let mut led = pins.led_rx.into_output();
 
-    let mut led = pins.led_rx.into_output();
-
-    loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
-    }
+//     loop {
+//         led.toggle();
+//         arduino_hal::delay_ms(1000);
+//     }
 }
