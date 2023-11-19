d5c9fd93b20dc1c44515309e097b54937932c765
nulvox <38874956+nulvox@users.noreply.github.com>
2023-11-18 22:18:31 -0500

    Sketch out main loop.

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,147 @@ static PAD_MASK_LEFT: u8 = 0x06;
 static PAD_MASK_UPLEFT: u8 = 0x07;
 static PAD_MASK_NONE: u8 = 0x08;
 
+// @TODO make this react to the pinout_config contents
+fn configure_input_pins(pinout_config) -> [&dyn InputPin<Error = core::convert::Infallible>; 16] {
+    let pins = arduino_hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut input_pins = [
+        pins.gpio0.into_pull_down_input(),
+        pins.gpio1.into_pull_down_input(),
+        pins.gpio2.into_pull_down_input(),
+        pins.gpio3.into_pull_down_input(),
+        pins.gpio4.into_pull_down_input(),
+        pins.gpio5.into_pull_down_input(),
+        pins.gpio6.into_pull_down_input(),
+        pins.gpio7.into_pull_down_input(),
+        pins.gpio8.into_pull_down_input(),
+        pins.gpio9.into_pull_down_input(),
+        pins.gpio10.into_pull_down_input(),
+        pins.gpio11.into_pull_down_input(),
+    ];
+
+    input_pins
+}
+
+read_pinout_config(pinout_config_path) -> ButtonPinout {
+    let mut file = File::open(pinout_config_path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let button_pinout: ButtonPinout = toml::from_str(&contents).unwrap();
+    button_pinout
+}
+
+fn get_report(pins: &[&dyn InputPin<Error = core::convert::Infallible>; 12]) -> SwitchGamepadReport {
+    // Read out 8 buttons first
+    let mut buttons = 0;
+    for (idx, &pin) in pins[..8].iter().enumerate() {
+        if pin.is_low().unwrap() {
+            buttons |= 1 << idx;
+        }
+    }
+    // @TODO actually get the values from our input pins
+
+    // We're using digital switches in a D-PAD style configuration
+    //    10
+    //  8    9
+    //    11
+    // These are mapped to the limits of an axis
+    // let x = if pins[8].is_low().unwrap() {
+    //     -127 // left
+    // } else if pins[9].is_low().unwrap() {
+    //     127 // right
+    // } else {
+    //     0 // center
+    // };
+
+    // let y = if pins[10].is_low().unwrap() {
+    //     -127 // up
+    // } else if pins[11].is_low().unwrap() {
+    //     127 // down
+    // } else {
+    //     0 // center
+    // };
+
+    SwitchGamepadReport { buttons, x, y }
+}
+
 #[arduino_hal::entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
-    let mut led = pins.led_rx.into_output();
+    let mut pac = pac::Peripherals::take().unwrap();
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let clocks = hal::clocks::init_clocks_and_plls(
+        bsp::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    info!("Starting");
+
+    //USB
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    let mut mumen = UsbHidClassBuilder::new()
+        .add_device(
+            usbd_human_interface_device::device::switch_gamepad::SwitchGamepadConfig::default(),
+        )
+        .build(&usb_bus);
+
+    //https://pid.codes
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0001))
+        .manufacturer("usbd-human-interface-device")
+        .product("Mumen Switch Controller")
+        .serial_number("0")
+        .build();
+
+    let mut led_pin = pins.gpio13.into_push_pull_output();
+    led_pin.set_high().ok();
+    
+    let pinout_config_path = "switchspec/d16s.toml";
+    let button_pinout = read_pinout_config(pinout_config_path);
+    //@TODO make this function work...
+    let input_pins = configure_input_pins(button_pinout);
+    input_copunt_down.start(1.millis());
+
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
+        // Poll every 10ms
+        if input_count_down.wait().is_ok() {
+            match mumen.device().write_report(&get_report(&input_pins)) {
+                Err(UsbHidError::WouldBlock) => {}
+                Ok(_) => {}
+                Err(e) => {
+                    core::panic!("Failed to write joystick report: {:?}", e)
+                }
+            }
+        }
+
+        if usb_dev.poll(&mut [&mut mumen]) {}
     }
 }
